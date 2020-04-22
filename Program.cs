ed207e17db15166949e59b476de6653d41c904cf
Tim Aldinger <tim.aldinger04@gmail.com>
2020-04-22 15:24:08 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SWAPI_TestCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
