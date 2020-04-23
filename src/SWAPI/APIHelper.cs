f64283ac31b77f0c62aa40087727f4bf15c7ea10
Tim Aldinger <tim.aldinger04@gmail.com>
2020-04-23 17:02:08 +0200

    Added URIs for all models

--- src/SWAPI/APIHelper.cs
+++ src/SWAPI/APIHelper.cs
@@ -7,16 +7,19 @@ namespace SWAPI_TestCs.src.SWAPI
     public static class APIHelper
     {
         public static HttpClient ApiClient { get; set; }
-        public static string URI_People = "https://swapi.dev/api/people/";
-        public static string URI_Vehicles = "https://swapi.dev/api/vehicles/";
+
+        internal static string URI_People = "https://swapi.dev/api/people/";
+        internal static string URI_Planets = "http://swapi.dev/api/planets/";
+        internal static string URI_Films = "http://swapi.dev/api/films/";
+        internal static string URI_Species = "http://swapi.dev/api/species/";
+        internal static string URI_Vehicles = "https://swapi.dev/api/vehicles/";
+        internal static string URI_Starships = "http://swapi.dev/api/starships/";
+
 
 
         public static void InitApiClient()
         {
-            ApiClient = new HttpClient
-            {
-                BaseAddress = new Uri("https://swapi.dev/api/")
-            };
+            ApiClient = new HttpClient();
             ApiClient.DefaultRequestHeaders.Accept.Clear();
             ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
