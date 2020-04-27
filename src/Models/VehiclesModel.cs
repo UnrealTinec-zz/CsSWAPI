0215d03a8c3164ac1b65b62b908c3bca6b42ded1
Tim Aldinger <tim.aldinger04@gmail.com>
2020-04-27 19:22:56 +0200

    Implemented User Input

--- src/Models/VehiclesModel.cs
+++ src/Models/VehiclesModel.cs
@@ -12,7 +12,7 @@ namespace SWAPI_TestCs.src.Models
         public string Model { get; set; }
         public string Manufacturer { get; set; }
         public string Cost_in_credits { get; set; }
-        public string Lenght { get; set; }
+        public string Length { get; set; }
         public string Max_atmosphering_speed { get; set; }
         public string Crew { get; set; }
         public string Passengers { get; set; }
@@ -21,7 +21,5 @@ namespace SWAPI_TestCs.src.Models
         public string Vehicle_class { get; set; }
         public List<string> Pilots { get; set; } //List with links
         public List<string> Films { get; set; } //List with links
- 
-
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -10,27 +10,50 @@ namespace SWAPI_TestCs
         static void Main(string[] args)
         {
             APIHelper.InitApiClient();
-            //--Tests--
-            PrintPeople(1,false);
-            PrintVehicles(24,true);
-            //---------
-            Console.ReadLine();
-        } //20.12.2014 22:30:21
+            Input:
+            Console.WriteLine("Category: - People\n" +
+                              "          - Planets\n" +
+                              "          - Vehicles");
+            Console.WriteLine("Choose Category: ");
+            string Catergory = Console.ReadLine();
+            Console.WriteLine("ID: ");
+            string IDinput = Console.ReadLine();
+            int ID;
 
-        private static async void PrintPeople(int id, bool appendCE)
+            if(!int.TryParse(IDinput, out ID)){
+                goto Input;
+            }
+
+            if (Catergory.ToLower().Equals("people"))
+            {
+                PrintPeople(ID);
+            }
+            else if (Catergory.ToLower().Equals("planets"))
+            {
+                throw new NotImplementedException();
+            }
+            else if (Catergory.ToLower().Equals("vehicles"))
+            {
+                PrintVehicles(ID);
+            }
+            Console.ReadLine();
+        } 
+
+        private static async void PrintPeople(int id = 1, bool appendCE = false)
         {
             StringBuilder msg;
             InfoProcessor infoProcessor = new InfoProcessor();
             var infos = await infoProcessor.LoadInfo<PeopleModel>(id);
-            msg = new StringBuilder($"-------------Character------------\n" +
-                $"Name: {infos.Name}\n" +
-                $"Height: {infos.Height}\n" +
-                $"Mass: {infos.Mass}\n" +
-                $"Hair color: {infos.Hair_color}\n" +
-                $"Skin color: {infos.Skin_color}\n" +
-                $"Eye color: {infos.Eye_color}\n" +
-                $"Birth year: {infos.Birth_year}\n" +
-                $"Gender: {infos.Gender}\n");
+            msg = new StringBuilder(
+                $"-------------Character------------\n" 
+                + $"Name: {infos.Name}\n" 
+                + $"Height: {infos.Height}\n" 
+                + $"Mass: {infos.Mass}\n" 
+                + $"Hair color: {infos.Hair_color}\n" 
+                + $"Skin color: {infos.Skin_color}\n" 
+                + $"Eye color: {infos.Eye_color}\n" 
+                + $"Birth year: {infos.Birth_year}\n" 
+                + $"Gender: {infos.Gender}\n");
 
             if (appendCE)
             {
@@ -41,7 +64,7 @@ namespace SWAPI_TestCs
             Console.WriteLine(msg);
 
         }
-        private static async void PrintVehicles(int id, bool appendCE)
+        private static async void PrintVehicles(int id = 4, bool appendCE = false)
         {
             StringBuilder msg;
             InfoProcessor infoProcessor = new InfoProcessor();
@@ -51,7 +74,7 @@ namespace SWAPI_TestCs
                 $"Model: {infos.Model}\n" +
                 $"Manufacturer: {infos.Manufacturer}\n" +
                 $"Cost in credits: {infos.Cost_in_credits}\n" +
-                $"Lenght: {infos.Lenght}\n" +
+                $"Lenght: {infos.Length}\n" +
                 $"Max atmosphering speed: {infos.Max_atmosphering_speed}\n" +
                 $"Crew: {infos.Crew}\n" +
                 $"Passengers: {infos.Passengers}\n" +
@@ -59,6 +82,7 @@ namespace SWAPI_TestCs
                 $"Consumables: {infos.Consumables}\n" +
                 $"Vehicle class: {infos.Vehicle_class}\n"
                 );
+
             if (appendCE)
             {
                 msg.Append($"Created: {infos.ConvertToLocalTime()[0]}\n" +
@@ -66,8 +90,17 @@ namespace SWAPI_TestCs
             }
             msg.Append($"--------------------------------\n");
             Console.WriteLine(msg);
+        }
 
+        /*
+        private static async void PrintInfos<T>(int id, bool appendCE) where T: Model
+        {
+            StringBuilder msg;
+            InfoProcessor infoProcessor = new InfoProcessor();
+            Type m = typeof(T);
+            var infos = await infoProcessor.LoadInfo<m>(id);
 
         }
+        */
     }
 }
